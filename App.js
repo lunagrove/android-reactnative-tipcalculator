6d934a7bdb897b27940a0640d0666ec391490daa
lunagrove <sholmes47@my.bcit.ca>
2023-04-03 21:39:47 -0700

    final version

--- App.js
+++ App.js
@@ -5,10 +5,19 @@ import { RadioButton } from 'react-native-paper';
 export default function App() {
   const [billAmount, setBillAmount] = useState(1.00);
   const [tipPercent, setTipPercent] = useState(15.0);
+
+  const generateRandomBill = () => {
+    const randomBillAmount = Math.floor(Math.random() * 400) + 1;
+    setBillAmount(randomBillAmount);
+  }
+
+  var tipAmount = tipPercent / 100 * billAmount;
+  var totalAmount = billAmount + tipAmount;
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Calculate Tip</Text>
-      <TouchableOpacity style={styles.button} onPress={() => setBillAmount(1.00)}>
+      <TouchableOpacity style={styles.button} onPress={generateRandomBill}>
         <Text style={styles.buttonText}>Generate Random Bill</Text>
       </TouchableOpacity>
       <Text style={styles.billAmount}>${billAmount.toFixed(2)}</Text>
@@ -18,34 +27,55 @@ export default function App() {
           value={10.0}
           status={tipPercent === 10.0 ? "checked" : "unchecked"}
           onPress={() => setTipPercent(10.0)}
+          color="white"
+          uncheckedColor="white"
         />
-        <Text>{'10%'}</Text>
+        <Text style={{color: '#ffffff', fontSize: 18}}>{'10%'}</Text>
       </View>
       <View style={styles.radioButtonRow}> 
         <RadioButton.Item
           value={15.0}
           status={tipPercent === 15.0 ? "checked" : "unchecked"}
           onPress={() => setTipPercent(15.0)}
+          color="white"
+          uncheckedColor="white"
         />
-        <Text>{'15%'}</Text>
+        <Text style={{color: '#ffffff', fontSize: 18}}>{'15%'}</Text>
       </View>
       <View style={styles.radioButtonRow}> 
         <RadioButton.Item
           value={18.0}
           status={tipPercent === 18.0 ? "checked" : "unchecked"}
           onPress={() => setTipPercent(18.0)}
+          color="white"
+          uncheckedColor="white"
         />
-        <Text>{'18%'}</Text>
+        <Text style={{color: '#ffffff', fontSize: 18}}>{'18%'}</Text>
       </View>
       <View style={styles.radioButtonRow}> 
         <RadioButton.Item
           value={20.0}
           status={tipPercent === 20.0 ? "checked" : "unchecked"}
           onPress={() => setTipPercent(20.0)}
+          color="white"
+          uncheckedColor="white"
         />
-        <Text>{'20%'}</Text>
+        <Text style={{color: '#ffffff', fontSize: 18}}>{'20%'}</Text>
+      </View>
+      <View style={styles.totalLine}>
+        <Text style={{...styles.totalTitle, marginTop: 20}}>Bill amount:</Text>
+        <Text style={{...styles.totalValue, marginTop: 20, marginBottom: 10}}>${billAmount.toFixed(2)}</Text>
       </View>
 
+      <View style={{...styles.totalLine, borderBottomWidth: 2, borderBottomColor: '#828787' }}>
+        <Text style={styles.totalTitle}>Tip amount:   ({tipPercent}%)</Text>
+        <Text style={styles.totalValue}>${tipAmount.toFixed(2)}</Text>
+      </View>
+      
+      <View style={styles.totalLine}>
+        <Text style={styles.totalTitle}>Total:</Text>
+        <Text style={styles.totalValue}>${totalAmount.toFixed(2)}</Text>
+      </View>
     </View>
   );
 }
@@ -85,7 +115,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     paddingTop: 15, 
-    marginBottom: 20,  
+    marginBottom: 30,  
   },
   radioButtonRow: {
     display: 'flex',
@@ -93,5 +123,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
     color: '#FFFFFF',
-  }
+  },
+  totalLine: {
+    marginTop: 8,
+    width: '98%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  totalTitle: {
+    color: '#31ebd8',
+    textAlign: 'right',
+    fontSize: 20,
+    marginBottom: 10,
+    marginRight: 20,
+    marginLeft: 50,
+  },
+  totalValue: {
+    color: '#31ebd8',
+    textAlign: 'right',
+    fontSize: 20,
+    marginRight: 50,
+  },
 });
